70206c988ed3bc4622b6889e27792fa34f4b57b5
jm5579 <joemartin7753@gmail.com>
2026-07-13 19:13:33 -0400

    Add ECR repository for application images

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,24 @@
 
 data "aws_caller_identity" "current" {}
 
+resource "aws_ecr_repository" "app" {
+  name                 = "secure-python-app"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name        = "secure-python-app"
+    Environment = var.environment
+  }
+}
+
 module "vpc" {
   source = "./modules/vpc"
 
