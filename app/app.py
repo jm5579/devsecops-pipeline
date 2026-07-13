514862aad8b571b76d1f4f28e6ab17ec012c4435
jm5579 <joemartin7753@gmail.com>
2026-07-13 18:48:17 -0400

    Add cross-origin embedder policy header

--- app/app.py
+++ app/app.py
@@ -56,6 +56,7 @@ def add_security_headers(response):
         "camera=(), microphone=(), geolocation=()"
     )
     response.headers["Cross-Origin-Opener-Policy"] = "same-origin"
+    response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
     response.headers["Cross-Origin-Resource-Policy"] = "same-origin"
     return response
 
